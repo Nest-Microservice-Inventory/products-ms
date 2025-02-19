c4a97520227c797c265edb89905102f3a9ea29c9
Mattias Duarte <duartemattias3@gmail.com>
2025-02-18 21:40:00 -0400

    microservice migration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config/envs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger("Products Microservice")
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule, 
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers
+      }
+    }
+  )
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,9 +26,7 @@ async function bootstrap() {
     })
   )
 
+  logger.log("Product Microservice running");
 
-  app.setGlobalPrefix('api');
-
-  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
